d45c14c1ee3275212535a98db161a0dbd23ed292
Hiraku Ohno <almichest.free@gmail.com>
2016-01-26 00:29:10 +0900

    Enable finding Hue bridge on network.

--- src/hue/BridgeScanner.py
+++ src/hue/BridgeScanner.py
@@ -1,2 +1,10 @@
-__author__ = 'hira'
+import requests
+import json
 
+
+def get_bridge_ips():
+    res = requests.get('http://www.meethue.com/api/nupnp').text
+    data = json.loads(res)
+    return [map['internalipaddress'] for map in data]
+
+print(get_bridge_ips())
