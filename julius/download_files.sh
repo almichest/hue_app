32dd87bd40d8fb4562467116360b5ea7232e25d6
Hiraku Ohno <almichest.free@gmail.com>
2016-02-27 21:12:09 +0900

    Add julius download script.

---
+++ julius/download_files.sh
@@ -0,0 +1,3 @@
+wget -O julius-4.3.1.tar.gz 'http://sourceforge.jp/frs/redir.php?m=osdn&f=%2Fjulius%2F60273%2Fjulius-4.3.1.tar.gz'
+wget -O dictation-kit-v4.3.1-linux.tgz 'http://sourceforge.jp/frs/redir.php?m=jaist&f=%2Fjulius%2F60416%2Fdictation-kit-v4.3.1-linux.tgz'
+wget -O grammar-kit-v4.1.tar.gz 'http://sourceforge.jp/frs/redir.php?m=osdn&f=%2Fjulius%2F51159%2Fgrammar-kit-v4.1.tar.gz'
